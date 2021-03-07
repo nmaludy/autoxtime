7a05ddd77bcec9db54e632492d519cbba716b209
Nick Maludy <nmaludy@gmail.com>
2021-03-07 01:33:47 -0500

    trying to get windows libraries linking

--- cmake/external_cutelyst.cmake
+++ cmake/external_cutelyst.cmake
@@ -4,6 +4,6 @@ ExternalProject_Add(external_cutelyst
   GIT_REPOSITORY "https://github.com/cutelyst/cutelyst.git"
   GIT_TAG "v2.14.2"
   LIST_SEPARATOR "|"
-  CMAKE_ARGS ${autoxtime_DEFAULT_ARGS} -DPLUGIN_VIEW_GRANTLEE=on -DPLUGIN_VIEW_CLEARSILVER=off  -DPLUGIN_VIEW_EMAIL=off
+  CMAKE_ARGS ${autoxtime_DEFAULT_ARGS} -DPLUGIN_VIEW_GRANTLEE=on -DPLUGIN_VIEW_CLEARSILVER=off  -DPLUGIN_VIEW_EMAIL=off -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=on
 )
 ExternalProject_Add_StepDependencies(external_cutelyst build external_grantlee)
