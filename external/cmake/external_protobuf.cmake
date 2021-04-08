641f983a48f10ebaf13084c94fd9ab2977489c84
Nick Maludy <nmaludy@gmail.com>
2021-04-08 10:14:40 -0400

    Adding extra compiler options

--- external/cmake/external_protobuf.cmake
+++ external/cmake/external_protobuf.cmake
@@ -2,11 +2,11 @@ ExternalProject_Add(external_protobuf
   PREFIX ${external_EXTERNAL_PREFIX}
   INSTALL_DIR ${external_INSTALL_PREFIX}
   GIT_REPOSITORY "https://github.com/protocolbuffers/protobuf.git"
-  GIT_TAG        "v3.15.5"
+  GIT_TAG        "v3.15.7"
   SOURCE_SUBDIR  cmake
   GIT_CONFIG advice.detachedHead=false
   LIST_SEPARATOR "|"
-  CMAKE_ARGS ${external_DEFAULT_ARGS} 
+  CMAKE_ARGS ${external_DEFAULT_ARGS}
   -Dprotobuf_BUILD_TESTS:BOOL=OFF
   -Dprotobuf_BUILD_PROTOC_BINARIES:BOOL=ON
   -Dprotobuf_BUILD_CONFORMANCE:BOOL=OFF
